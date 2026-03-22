a293099677482df077513482872866c1dfcf26c1
kukkapallidurgaprasad <149488450+kukkapallidurgaprasad@users.noreply.github.com>
2026-03-22 23:56:34 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.title("Bitcoin Price Prediction")
+
+st.write("This project predicts Bitcoin prices using ML")
+
+st.write("Upload your dataset or integrate your model here")
